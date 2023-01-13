ac0a9ebbab59d8c7ce51da670e9b831c6f80c8de
Luke Archbold <1024504@student.sae.edu.au>
2023-01-13 16:35:46 +1100

    Luke Update
    Movement System Added
    Player Prefab Created

--- Assets/Team Members/Luke/Scripts/Player.cs
+++ Assets/Team Members/Luke/Scripts/Player.cs
@@ -1,24 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	[Header("Design")]
+	public float moveSpeed;
+	public float jumpSpeed;
+	public float jumpTime;
+	public float gravityScale; 
+	public LayerMask groundLayerMask;
 
+	[Header("Logic")]
+	public float lateralMoveInput;
+	[SerializeField] private bool _isJumping;
+	private float _distanceToGround;
+	private Coroutine _coroutine;
 
+	private Transform _transform;
+	private Rigidbody2D _rb;
 
-
-
-
-
-
-	public void Move(Vector2 moveInput)
+	private void OnEnable()
 	{
-		
+		_transform = transform;
+		_rb = GetComponent<Rigidbody2D>();
+		_rb.gravityScale = gravityScale;
+		_distanceToGround = GetComponent<Collider2D>().bounds.extents.y;
+	}
+
+	private void FixedUpdate()
+	{
+		Move(lateralMoveInput);
 	}
 	
-	public void Jump(Vector2 moveInput)
+	private void Move(float input)
 	{
-		
+		_rb.velocity = new Vector2(Mathf.Ceil(input) * moveSpeed, _rb.velocity.y);
 	}
+	
+	public void Jump()
+	{
+		if (!IsOnGround) return;
+		if (_coroutine != null) StopCoroutine(_coroutine);
+		_isJumping = true;
+		_rb.gravityScale = 0f;
+		_rb.velocity = new Vector2(_rb.velocity.x, jumpSpeed);
+		_coroutine = StartCoroutine(JumpTimer());
+	}
+
+	public void CancelJump()
+	{
+		if (!_isJumping) return;
+		if (_coroutine != null) StopCoroutine(_coroutine);
+		_isJumping = false;
+		_rb.gravityScale = gravityScale;
+		if (_rb.velocity.y > 0) _rb.velocity = new Vector2(_rb.velocity.x, 0);
+	}
+
+	private IEnumerator JumpTimer()
+	{
+		float decelerationDuration = jumpSpeed / gravityScale / Physics.gravity.magnitude;
+		StartCoroutine(CeilingCheck());
+		yield return new WaitForSeconds(jumpTime - decelerationDuration);
+		_rb.gravityScale = gravityScale;/*
+		yield return new WaitForSeconds(decelerationDuration);
+		CancelJump();*/
+	}
+
+	private IEnumerator CeilingCheck()
+	{
+		yield return new WaitUntil(() => _rb.velocity.y <= 0);
+		if (_coroutine != null) CancelJump();
+	}
+	
+	public bool IsOnGround => Physics2D.Raycast(_transform.position, Vector2.down, _distanceToGround + 0.1f, groundLayerMask);
 }
--- Assets/Team Members/Luke/Scripts/PlayerController.cs
+++ Assets/Team Members/Luke/Scripts/PlayerController.cs
@@ -15,15 +15,48 @@ public class PlayerController : MonoBehaviour
 
 	private void OnEnable()
 	{
+		_controls = new();
+		_controls.UI.Disable();
+
+		_moveInput = _controls.Player.Move;
 		_moveInput.Enable();
+		_moveInput.performed += MovePerformed;
+		_moveInput.canceled += MoveCancelled;
+		
+		_jumpInput = _controls.Player.Jump;
 		_jumpInput.Enable();
+		_jumpInput.performed += JumpPerformed;
+		_jumpInput.canceled += JumpCancelled;
 	}
 	
 	private void OnDisable()
 	{
 		_moveInput.Disable();
+		_moveInput.performed -= MovePerformed;
+		_moveInput.canceled -= MoveCancelled;
+
 		_jumpInput.Disable();
+		_jumpInput.performed -= JumpPerformed;
+		_jumpInput.canceled -= JumpCancelled;
+	}
+
+	private void MovePerformed(InputAction.CallbackContext context)
+	{
+		_player.lateralMoveInput = context.ReadValue<Vector2>().x;
+	}
+
+	private void MoveCancelled(InputAction.CallbackContext context)
+	{
+		_player.lateralMoveInput = 0;
+	}
+
+	private void JumpPerformed(InputAction.CallbackContext context)
+	{
+		_player.Jump();
 	}
 	
-	
+	private void JumpCancelled(InputAction.CallbackContext context)
+	{
+		_player.CancelJump();
+	}
 }
