d3e73d3eacc6ce693f5ce6a1e9a65c6d10043c39
Luke Archbold <1024504@student.sae.edu.au>
2023-01-13 14:47:25 +1100

    Added Action Map

--- Assets/Team Members/Luke/Scripts/PlayerController.cs
+++ Assets/Team Members/Luke/Scripts/PlayerController.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private PlayerControls _controls;
+	private InputAction _moveInput;
+	private InputAction _jumpInput;
+	// Add attack/interact/etc.
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+	[SerializeField] private Player _player;
+
+	private void OnEnable()
+	{
+		_moveInput.Enable();
+		_jumpInput.Enable();
+	}
+	
+	private void OnDisable()
+	{
+		_moveInput.Disable();
+		_jumpInput.Disable();
+	}
+	
+	
 }
