54013112e9b8863c2476b9fccdb37f127b60aa9b
SetearaKevinOm <91643261+SetearaKevinOm@users.noreply.github.com>
2023-02-20 15:40:32 +1100

    some weapon stuff

--- Assets/Team Members/Luke/Scripts/Player.cs
+++ Assets/Team Members/Luke/Scripts/Player.cs
@@ -35,7 +35,9 @@ public class Player : MonoBehaviour, IControllable
 	public List<GameObject> weaponsList;
 	public List<GameObject> bulletList;
 	public List<Transform> barrelTransforms;
+	public GameObject playerArms;
 
+	public Vector3 mousePos;
 	//public Camera camera;
 
 	private Transform _transform;
@@ -51,21 +53,28 @@ public class Player : MonoBehaviour, IControllable
 		_rb.gravityScale = gravityScale;
 		_terrainCollider = GetComponentInChildren<TerrainCollider>();
 		_grapple = GetComponentInChildren<Grapple>(true);
+
+		
 	}
 
 	private void FixedUpdate()
 	{
 		Move(_lateralMoveInput);
-
-		Vector3 gunPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		if (gunPos.x < transform.position.x)
+		mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		//Vector3 gunPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		/*if (mousePos.x < transform.position.x)
 		{
 			gameObject.transform.eulerAngles = new Vector3(transform.rotation.x, 180f, transform.rotation.z);
 		}
 		else
 		{
 			gameObject.transform.eulerAngles = new Vector3(transform.rotation.x, 0f, transform.rotation.z);
-		}
+		}*/
+
+		Vector3 rotation = mousePos - transform.position;
+		float rotationZ = Mathf.Clamp((Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg),-70f,230f);
+		playerArms.transform.rotation = Quaternion.Euler(0, 0, rotationZ);
+
 	}
 	
 	private void Move(float input)
