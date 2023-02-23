806d7826cdb887f8d90ee2c8b2d9d62c0bce3d86
Luke Archbold <1024504@student.sae.edu.au>
2023-02-24 10:39:37 +1100

    Luke A Update - Camera Tracker Bounds created as prefab

--- Assets/Team Members/Luke/Scripts/CameraTracker.cs
+++ Assets/Team Members/Luke/Scripts/CameraTracker.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 
 public class CameraTracker : MonoBehaviour
 {
+	private Camera _cam;
 	private Transform _cameraTransform;
 	public Transform playerTransform;
+	private BoxCollider2D _collider;
 
 	[Range(0,5)]
 	public float cameraSpeed = 1f;
 	public float cameraDistance = 10f;
 
 	private void OnEnable()
-    {
+	{
+		_cam = GetComponent<Camera>();
 	    _cameraTransform = transform;
+	    _collider = GetComponent<BoxCollider2D>();
+	    UpdateCollider();
     }
 
     private void FixedUpdate()
     {
 	    _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, playerTransform.position+cameraDistance*Vector3.back, 0.1f*cameraSpeed);
+    }
+
+    private void UpdateCollider()
+    {
+	    _collider.size = new Vector2(2*_cam.orthographicSize*_cam.aspect, 2*_cam.orthographicSize);
     }
 }
