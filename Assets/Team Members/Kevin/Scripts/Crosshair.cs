a688237ac372eac16486e8643de6492e627c72aa
SetearaKevinOm <1024325@student.sae.edu.au>
2023-03-22 22:24:58 +1100

    sniper bullet fix, some crosshair work

--- Assets/Team Members/Kevin/Scripts/Crosshair.cs
+++ Assets/Team Members/Kevin/Scripts/Crosshair.cs
@@ -5,22 +5,45 @@ using UnityEngine;
 
 public class Crosshair : MonoBehaviour
 {
+    public bool onTarget = false;
+    
     public void OnTriggerStay2D(Collider2D col)
     {
         AlveriumSoldier enemy = col.GetComponent<AlveriumSoldier>();
         if (enemy != null)
         {
-            gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+            onTarget = true;
+            gameObject.GetComponent<SpriteRenderer>().color = Color.red;
         }
     }
 
     public void OnTriggerExit2D(Collider2D other)
     {
-        gameObject.GetComponent<SpriteRenderer>().color = Color.red;
-        /*EnemyBody enemy = other.GetComponent<EnemyBody>();
-        if (enemy != null)
-        {
-            
-        }*/
+        onTarget = false;
+        gameObject.GetComponent<SpriteRenderer>().color = Color.cyan;
     }
+
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(1.5f);
+    }
+    
+    public void Update()
+    {
+        if (onTarget)
+        {
+            gameObject.transform.rotation =
+                Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, -45f), 10f);
+            gameObject.transform.localScale = new Vector3(0.0630299002f, 0.0630299002f, 0.0630299002f) * 2;
+            
+            
+        }
+        else if(!onTarget)
+        {
+            gameObject.transform.rotation =
+                Quaternion.RotateTowards(transform.rotation,Quaternion.Euler(0, 0, 0) , 10f);
+            gameObject.transform.localScale = new Vector3(0.0630299002f, 0.0630299002f, 0.0630299002f);
+        }
+    }
+    
 }
--- Assets/Team Members/Kevin/Scripts/SniperBullet.cs
+++ Assets/Team Members/Kevin/Scripts/SniperBullet.cs
@@ -4,12 +4,5 @@ using UnityEngine;
 
 public class SniperBullet : BulletBase
 {
-    public override void OnHit(Collision2D col)
-    {
-        EnemyBody enemy = col.gameObject.GetComponent<EnemyBody>();
-        if (enemy != null)
-        {
-            enemy.health.HealthLevel -= bulletDmg;
-        }
-    }
+    
 }
