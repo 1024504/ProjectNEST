f4975b748c4bdf659cdfeefe663c46c9e21f751c
Bryce Sandall <86949903+bsandall2@users.noreply.github.com>
2023-03-24 17:56:05 +1100

    Medkit pickup sounds

--- Assets/Team Members/Kevin/Scripts/Medkit.cs
+++ Assets/Team Members/Kevin/Scripts/Medkit.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 
 public class Medkit : MonoBehaviour
 {
+    public FMODUnity.EventReference pickUpRef;
+    public FMODUnity.EventReference emoteRef;
+
     public void OnTriggerEnter2D(Collider2D col)
     {
         Player player = col.GetComponent<Player>();
         if (player != null && player.medkitCount < player.maxMedkit)
         {
-            FMODUnity.RuntimeManager.PlayOneShot("event:/Characters/Interactables/PickUpMedkitSFX/PickUpMedkit");
-            FMODUnity.RuntimeManager.PlayOneShot("event:/Characters/Dialogue/Falcon/Emotes/Medkit_Pickup");
+            StartCoroutine(EmoteDelay());
+            FMODUnity.RuntimeManager.PlayOneShot(emoteRef);                         
             player.PickUp();
-            Destroy(gameObject);
+            gameObject.transform.position = new Vector2(937, 123);
+            Destroy(gameObject, 1f);
         }
+    }
+
+    public IEnumerator EmoteDelay()
+    {
+        yield return new WaitForSeconds(.5f);
+        FMODUnity.RuntimeManager.PlayOneShot(pickUpRef);
     }
 }
