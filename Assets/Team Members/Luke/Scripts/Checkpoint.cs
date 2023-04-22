f09ebaf041fd9c651adae1603559f8bce026f930
Luke Archbold <1024504@student.sae.edu.au>
2023-04-22 22:29:30 +1000

    Luke A Update - Checkpoint bug solved

--- Assets/Team Members/Luke/Scripts/Checkpoint.cs
+++ Assets/Team Members/Luke/Scripts/Checkpoint.cs
@@ -8,6 +8,8 @@ public class Checkpoint : MonoBehaviour
 {
 	private void OnTriggerEnter2D(Collider2D col)
 	{
+		Player player = col.GetComponent<Player>();
+		if (player == null) return;
 		if (GameManager.Instance.saveData.playerPosition == transform.position) return;
 		GameManager.Instance.SaveCheckpoint(this);
 	}
