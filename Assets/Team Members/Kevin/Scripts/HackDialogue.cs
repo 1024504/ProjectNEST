3b3d4cca6e48f5e68af993d32eead798b5bc2649
SetearaKevinOm <1024325@student.sae.edu.au>
2023-04-25 22:37:56 +1000

    tutorial

--- Assets/Team Members/Kevin/Scripts/HackDialogue.cs
+++ Assets/Team Members/Kevin/Scripts/HackDialogue.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+
 public class HackDialogue : MonoBehaviour
 {
     public EventReference dialogue;
+    public ParticleSystem ParticleSystem;
+    
     private void OnDisable()
     {
         RuntimeManager.PlayOneShot(dialogue);
