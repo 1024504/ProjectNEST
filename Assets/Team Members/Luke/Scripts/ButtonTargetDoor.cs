2b0640169bd6f5f124984f809013ee1b4f29409e
SetearaKevinOm <1024325@student.sae.edu.au>
2023-04-25 15:04:14 +1000

    HangarDoorTimeline Dialogue added

--- Assets/Team Members/Luke/Scripts/ButtonTargetDoor.cs
+++ Assets/Team Members/Luke/Scripts/ButtonTargetDoor.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
-public class ButtonDoor : MonoBehaviour
+public class ButtonTargetDoor : MonoBehaviour
 {
     public InteractableObject trigger;
-
+    
     private void OnEnable()
     {
 	    if (trigger != null)
--- Assets/Team Members/Luke/Scripts/InteractableButton.cs
+++ Assets/Team Members/Luke/Scripts/InteractableButton.cs
@@ -1,15 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using FMODUnity;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class InteractableButton : InteractableObject
 {
 	[SerializeField] GameObject buttonUI;
+	public PlayableDirector timeLineDirector;
+	public EventReference interactSFX;
+	public EventReference dialogueAudio;
+
+	public void OnEnable()
+	{
+		timeLineDirector.played += PlayCutScene;
+		timeLineDirector.stopped += StopCutScene;
+	}
 	
 	protected override void Interact()
 	{
 		OnInteract?.Invoke();
+		RuntimeManager.PlayOneShot(interactSFX);
+		RuntimeManager.PlayOneShot(dialogueAudio);
+		timeLineDirector.Play();
 		if (objective != GameManager.Objectives.None)
 		{
 			GameManager.Instance.UpdateObjective(objective);
@@ -21,5 +36,15 @@ public class InteractableButton : InteractableObject
 		}
 	}
 
+	public void PlayCutScene(PlayableDirector obj)
+	{
+		GameManager.Instance.playerController.Controls.Disable();
+		Debug.Log("Start");
+	}
 	
+	public void StopCutScene(PlayableDirector obj)
+	{
+		GameManager.Instance.playerController.Controls.Enable();
+		Debug.Log("Stop");
+	}
 }
