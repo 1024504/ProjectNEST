d3fa27e03509168e58fc04eca915ba20c9d54ab1
Bryce Sandall <1024240@student.sae.edu.au>
2023-04-26 16:01:12 +1000

    Cutscene Borders

---
+++ Assets/Team Members/Bryce/UI_Art/Cutscenes/CutsceneBorder_Script.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneBorder_Script : MonoBehaviour
+{
+	public GameObject cutsceneBorderPrefab;
+
+	public Animator borderANIMATOR;
+
+	public bool cutsceneFinished;
+
+	public void Update()
+	{
+		CutsceneFinished();
+	}
+
+	public void CutsceneFinished()
+	{
+		if (cutsceneFinished == true)
+		{
+			borderANIMATOR.CrossFade("Border_OUT", 0, 0);
+		}
+	}
+	
+	public void borderEnable()
+	{
+		cutsceneBorderPrefab.SetActive(true);
+	}
+	
+	public void DisableBorder()
+	{
+		if (cutsceneFinished == true)
+		{
+			cutsceneBorderPrefab.SetActive(false);
+			cutsceneFinished = false;
+		}
+	}
+}
