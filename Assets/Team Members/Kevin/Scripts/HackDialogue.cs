e42d69291ef763eca2d6047ccceb9675a6f7731a
SetearaKevinOm <1024325@student.sae.edu.au>
2023-04-27 00:58:45 +1000

    first kill timeline

--- Assets/Team Members/Kevin/Scripts/HackDialogue.cs
+++ Assets/Team Members/Kevin/Scripts/HackDialogue.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using UnityEngine.Playables;
 
 public class HackDialogue : MonoBehaviour
 {
-    public EventReference dialogue;
-
-    private void OnDisable()
+    public PlayableDirector timeLineDirector;
+    
+    public void OnDisable()
     {
-        RuntimeManager.PlayOneShot(dialogue);
+        timeLineDirector.Play();
     }
 }
