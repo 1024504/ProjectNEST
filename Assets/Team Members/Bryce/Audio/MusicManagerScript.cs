8b241fe97d8f648357a7ff975a503fb9f91ac885
Bryce Sandall <1024240@student.sae.edu.au>
2023-04-27 12:39:16 +1000

    Audio manager for Unity Setup

---
+++ Assets/Team Members/Bryce/Audio/MusicManagerScript.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class MusicManagerScript : MonoBehaviour
+{
+	public FMODUnity.EventReference fmodEvent;
+	private FMOD.Studio.EventInstance music;
+	
+	private int trackSelector;
+
+	public int TrackSelector
+	{
+		get => trackSelector;
+		
+		set
+		{
+			if (value == trackSelector) return;
+			trackSelector = value;
+			UpdateParameter();
+		}
+	}
+
+	public void Start()
+	{
+		music = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+		music.start();
+	}
+
+	private void UpdateParameter()
+	{
+		music.setParameterByName("TrackSelection", trackSelector);
+	}
+}
