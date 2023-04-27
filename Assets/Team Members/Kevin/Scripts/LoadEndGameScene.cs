c9b97fe9f3992895be26663f24320ba91ab828f3
SetearaKevinOm <1024325@student.sae.edu.au>
2023-04-27 22:09:24 +1000

    end game signals

---
+++ Assets/Team Members/Kevin/Scripts/LoadEndGameScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadEndGameScene : MonoBehaviour
+{
+    public void LoadEndScene()
+    {
+        Debug.Log("FINALLY FINISH!!!");
+    }
+    
+}
