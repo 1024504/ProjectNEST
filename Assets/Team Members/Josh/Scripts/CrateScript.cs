5c925c5bc0b61ed7cbd377dbb3d99228a6bc8073
LgJoshi <80075442+LgJoshi@users.noreply.github.com>
2023-05-02 18:24:50 +1000

    Misc Stuff
    adjusted Moth projectile lines
    adjusted Boss projectile lines
    changed Destructible Crates logic and added swapping sprites
    small plaza touchups
    - added stationary grapple hooks
    - added obstacle crates
    - nerfed a jump by 'extending' a platform

--- Assets/Team Members/Josh/Scripts/CrateScript.cs
+++ Assets/Team Members/Josh/Scripts/CrateScript.cs
@@ -9,12 +9,59 @@ public class CrateScript : MonoBehaviour, IDestructable
     public GameObject halfCrate;
     public GameObject alveriumPrefab;
     public GameObject medkitPrefab;
+    [SerializeField] int loadedChance = 26;
+
+    [SerializeField] Sprite undamagedSprite;
+    [SerializeField] Sprite damagedSprite;
+    [SerializeField] SpriteRenderer crateSpriteRenderer;
+    [SerializeField] CrateState myCrateState = CrateState.normal;
 
     public FMODUnity.EventReference explodeRef;
 
+    enum CrateState
+    {
+        normal,
+        alverium,
+        medkit
+    }
+
     private void Start()
     {
-        isLoaded = Random.Range(0, 2);
+        switch (myCrateState){
+            case CrateState.alverium:
+                isLoaded = 1;
+                break;
+
+            case CrateState.medkit:
+                isLoaded = 0;
+                break;
+
+            case CrateState.normal:
+                isLoaded = Random.Range(0, 2);
+                break;
+
+            default:
+                print("crate destructible start uh oh");
+                break;
+        }
+
+        int coinFlip = Random.Range(0, 100);
+        if( coinFlip > loadedChance )
+        {
+            isLoaded = 1;
+        } else
+        {
+            isLoaded = 0;
+        }
+
+        if( isLoaded == 1 )
+        {
+            crateSpriteRenderer.sprite = damagedSprite;
+        } else
+        {
+            crateSpriteRenderer.sprite = undamagedSprite;
+        }
+
     }
 
     public void Destroyed()
@@ -23,18 +70,18 @@ public class CrateScript : MonoBehaviour, IDestructable
         //FMODUnity.RuntimeManager.PlayOneShot(explodeRef);
 
         Instantiate(halfCrate, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
-        int coinFlip = Random.Range(0, 100);
-        if(coinFlip > 26)
+
+        if( isLoaded==1 )
         {
             GameObject go = Instantiate(alveriumPrefab,
                 new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
             HealthBase newbornHp = go.GetComponent<HealthBase>();
             go.transform.localScale = Vector3.one / 1.5f;
             go.GetComponent<AlveriumSoldier>().attackDamage = 10;
-            newbornHp.HealthLevel = newbornHp.maxHealth / 2;
             //damage alverium
+            newbornHp.HealthLevel = newbornHp.maxHealth / 2;
         }
-        else if (coinFlip <= 25) 
+        else if ( isLoaded == 0 ) 
         {
             Instantiate(medkitPrefab, transform.position, Quaternion.identity);
         }
